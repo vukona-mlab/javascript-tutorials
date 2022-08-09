d1662eeeb0aeb73a8f68b93fad03ee38936fb2b4
vukona <vukona@mlab.co.za>
2022-08-09 19:34:56 +0200

    lesson-01

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+let string = 'I am a string';
+let number = 0;
+let bln = true;
+
+console.group('index');
+console.log(string);
+console.log(number);
+console.log(bln);
+console.groupEnd()
+console.log('hi');
